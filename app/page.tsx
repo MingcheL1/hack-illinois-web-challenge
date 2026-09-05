8549c7c1e9b96f40051dcc3807d996a0d6c78d99
Mingche <m1ngch3.li@gmail.com>
2026-09-05 12:38:48 -0500

    temp to save work

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Navbar } from "@/components/navbar";
+import { Surface } from "@/components/surface";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="bg-blue-400 min-h-screen">
       <Navbar/>
+      <Surface/>
     </div>
   )
 }
---
+++ components/surface.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+export const Surface: FC = () => {
+    return(
+        <div>
+            <div className="">
+                <h1>HackIllinois 2027</h1>
+                <p>Take a Deep Dive into Our Schedule</p>
+
+            </div>
+        </div>
+    );
+}
