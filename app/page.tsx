532870aa1c6497cb58e40f58e256247ede60e1bd
MingcheL1 <m1ngch3.li@gmail.com>
2026-09-05 16:39:47 -0500

    switched to props

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,31 @@
+"use client";
 import { Events } from "@/components/events";
 import { Navbar } from "@/components/navbar";
 import { Surface } from "@/components/surface";
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+    const [data, setData]=useState<unknown>(null); //can't interact until I know what the variable type is
+    useEffect(()=>{
+        async function loadEvents(){
+            try{
+                const response= await fetch("https://adonix.hackillinois.org/event/")
+                const json: unknown=(await response.json());
+                setData(json);
+            }
+            catch(error){
+                console.log("booooo");
+            }
+            
+        }
+        loadEvents();
+    },[])
   return (
     <div className="sky min-h-screen">
       <Navbar/>
       <Surface/>
-      <Events/>
+      <Events data={data}/>
     </div>
   )
 }
--- components/events.tsx
+++ components/events.tsx
@@ -1,25 +1,15 @@
 "use client"
 import { FC, useEffect, useState } from "react";
-
-export const Events: FC=()=>{
-    const [data, setData]=useState<unknown>(null); //can't interact until I know what the variable type is
-    useEffect(()=>{
-        async function loadEvents(){
-            try{
-                const response= await fetch("https://adonix.hackillinois.org/event/")
-                const json: unknown=(await response.json());
-                setData(json);
-            }
-            catch(error){
-                console.log("booooo")
-            }
-            
-        }
-        loadEvents();
-    },[])
+type eventProps={
+    data: unknown;
+}
+export const Events: FC<eventProps>=({data})=>{
     return(
         <div>
             <p>{JSON.stringify(data,null,2)}</p>
         </div>
     );
 }
+
+//map events next
+//dive mode vs compact mode
