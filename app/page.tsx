6240a734c5e716a316212bfc5c1a7d91db6b62a5
Mingche <m1ngch3.li@gmail.com>
2026-09-06 11:16:23 -0500

    simple mapping

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,25 @@
 import { Events } from "@/components/events";
 import { Navbar } from "@/components/navbar";
 import { Surface } from "@/components/surface";
+import { EventData } from "@/types/event";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 
+
 export default function Home() {
-    const [data, setData]=useState<unknown>(null); //can't interact until I know what the variable type is
+    const [data, setData]=useState<EventData|null>(null); //can't interact until I know what the variable type is
     useEffect(()=>{
         async function loadEvents(){
             try{
                 const response= await fetch("https://adonix.hackillinois.org/event/")
-                const json: unknown=(await response.json());
+                if(!response.ok){
+                    throw new Error("Failed to fetch events");
+                }
+                const json: EventData=(await response.json());
                 setData(json);
             }
             catch(error){
-                console.log("booooo");
+                console.log(error);
             }
             
         }
--- components/events.tsx
+++ components/events.tsx
@@ -1,12 +1,19 @@
 "use client"
+import { EventData } from "@/types/event";
 import { FC, useEffect, useState } from "react";
 type eventProps={
-    data: unknown;
+    data: EventData|null;
 }
 export const Events: FC<eventProps>=({data})=>{
     return(
         <div>
-            <p>{JSON.stringify(data,null,2)}</p>
+            {
+                data?.events.map((event)=>(
+                    <div key={event.eventID} className="bg-white text-black rounded-lg shadow-md p-4 m-4">
+                        <h2 className="text-xl font-bold">{event.name}</h2>
+                    </div>
+                ))
+            }
         </div>
     );
 }
