79f6a0acd80c8c7b64f8f0abd0c9ba53c2f43dff
MingcheL1 <m1ngch3.li@gmail.com>
2026-09-06 22:38:49 -0500

    updated UI

--- components/compact.tsx
+++ components/compact.tsx
@@ -11,14 +11,14 @@ export const Compact: FC<eventProps> = ({ data }) => {
         <img
             src="/images/compactBG.jpg"
             alt="Compact Mode"
-            className="w-full h-full object-cover absolute blur-[3px]"
+            className="w-full h-full object-cover absolute inset-0"
         />
-        <div className="inline-flex gap-2 mb-8 p-2">
+        <div className="inline-flex gap-2 ml-8 mt-10 mb-8 p-2 relative">
             {
                 ["Friday", "Saturday", "Sunday"].map((d) => {
                     const active=day===d;
                     return(
-                        <button key={d} onClick={ ()=> {setDay(d); console.log(d)}} className={`px-4 py-2 rounded-lg backdrop-blur-sm ${active ? 'bg-black/30' : 'bg-black/20 hover:bg-black/30'}`}>
+                        <button key={d} onClick={ ()=> {setDay(d); console.log(d)}} className={`px-4 py-2 rounded-lg ${active ? 'bg-yellow-300 text-black' : 'bg-blue-900/80 text white hover:bg-yellow-300/75'}`}>
                             {d}
                         </button>
                     );
@@ -28,13 +28,13 @@ export const Compact: FC<eventProps> = ({ data }) => {
 
         <div className="absolute inset-0 mt-20">
 
-            <div className="relative mt-8 max-w-full overflow-hidden rounded-md border border-white/30 bg-white10 p-6 shadow-2xl backdrop-blur-2xl">
+            <div className="relative mt-8 w-[850px] ml-10 overflow-hidden rounded-md border border-white/49 bg-blue-900/80 backdrop-blur-[2px] p-6 ">
 
 
 
                 <div className="relative z-10">
                     <div className="mb-6 flex items-end justify-between">
-                        <h1 className="text-lg uppercase tracking-[0.25em] text-white/60">Schedule</h1>
+                        <h1 className="text-lg uppercase text-white/60">Schedule</h1>
                         <p className="text-sm text-white/60">{day}</p>
                     
                     </div>
@@ -43,11 +43,13 @@ export const Compact: FC<eventProps> = ({ data }) => {
                 {filteredEvents?.map((event) => (
                     <div
                         key={event.eventID}
-                        className=" text-white rounded-md shadow-md p-4 m-4"
+                        className="text-white rounded-md shadow-md m-4 border border-white/10 shadow-lg bg-blue-800/70 px-5 py-4"
                     >
                         
-                            
-                        <h2 className="text-xl font-bold">{convertTime(event.startTime)} - {convertTime(event.endTime)}</h2>
+                        <h1 className="text-xl font-bold">{event.name}</h1>    
+                        <h2 className="mb-1 text-sm font-bold">{convertTime(event.startTime)} - {convertTime(event.endTime)}</h2>
+                        <h3 className="mt-1 text-sm text-white">{event.locations.description}</h3>
+                        <p className="text-white">{event.description}</p>
                     </div>
                 ))} 
             </div>
